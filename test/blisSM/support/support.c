8e2cffbcd9be04d0ba50ce04f65518f4eeec9a8a
Bryan Marker <bamarker@cs.utexas.edu>
2013-08-01 12:43:54 -0500

    towards blis thread communicators

---
+++ test/blisSM/support/support.c
@@ -0,0 +1,102 @@
+#include "support.h"
+#include "math.h"
+
+rank_t th_group_id( thread_comm_t *comm )
+{
+  return floor( (double)th_global_thread_id() / comm->num_threads );
+}
+
+rank_t th_thread_id( thread_comm_t *comm )
+{
+    return th_global_thread_id() % comm->num_threads;
+}
+
+rank_t th_global_thread_id()
+{
+    return omp_get_thread_num();
+}
+
+//I have armies of threads and need officers to organize them
+bool_t th_am_root( thread_comm_t *comm )
+{
+    return th_thread_id( comm ) == 0;
+}
+
+void th_cleanup_communicator( thread_comm_t *comm )
+{
+    if( comm == NULL ) return;
+    th_destroy_lock( &comm->barrier_lock );
+}
+
+void th_setup_communicator( thread_comm_t *comm, dim_t num_threads)
+{
+    if( comm == NULL ) return;
+    comm->sent_object = NULL;
+    comm->num_threads = num_threads;
+    comm->barrier_sense = 0;
+    th_init_lock( &comm->barrier_lock );
+    comm->barrier_threads_arrived = 0;
+}
+
+void* th_broadcast_structure( thread_comm_t *comm, void *to_send )
+{   
+    if( comm == NULL ) return to_send;
+
+    if( th_am_root( comm ) )
+        comm->sent_object = to_send;
+    th_barrier( comm );
+    void  *object = comm->sent_object;
+    th_barrier( comm );
+    return object;
+}
+
+void th_init_lock( lock_t *lock )
+{
+    omp_init_lock( lock );
+}
+void th_destroy_lock( lock_t *lock )
+{
+    omp_destroy_lock( lock );
+}
+void th_set_lock( lock_t *lock )
+{
+    omp_set_lock( lock );
+}
+void th_unset_lock( lock_t *lock )
+{
+    omp_unset_lock( lock );
+}
+
+//barrier routine taken from art of multicore programming or something
+void th_barrier( thread_comm_t *comm )
+{
+    if(comm == NULL)
+        return;
+    bool_t my_sense = comm->barrier_sense;
+    dim_t my_threads_arrived;
+    th_set_lock(&comm->barrier_lock);
+    my_threads_arrived = comm->barrier_threads_arrived + 1;
+    comm->barrier_threads_arrived = my_threads_arrived;
+    th_unset_lock(&comm->barrier_lock);
+    if( my_threads_arrived == comm->num_threads ) {
+        comm->barrier_threads_arrived = 0;
+        comm->barrier_sense = !comm->barrier_sense;
+        bool_t *mouth = &comm->barrier_sense;
+        _Pragma("omp flush ( mouth )")
+    }
+    else {
+        volatile bool_t *listener = &comm->barrier_sense;
+        while( *listener == my_sense ) {}
+    }
+}
+
+void th_shift_start_end(dim_t *start, dim_t *end, thread_comm_t *comm)
+{
+  rank_t rank = th_thread_id( comm );
+  dim_t len = *end - *start;
+  dim_t n_pt = len / comm->num_threads;
+  n_pt = (n_pt * comm->num_threads < len) ? n_pt + 1 : n_pt;
+  n_pt = (n_pt % 4 == 0) ? n_pt : n_pt + 4 - (n_pt % 4);
+  *start = *start + rank * n_pt;
+  *end   = bli_min( *start + n_pt, start + len );
+}
---
+++ test/blisSM/support/support.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <unistd.h>
+#include "blis.h"
+#include <pthread.h>
+
+typedef unsigned int rank_t;
+typedef unsigned int thread_count_t;
+typedef omp_lock_t lock_t;
+
+
+typedef struct
+{
+  void*   sent_object;
+  thread_count_t num_threads;
+  thread_count_t num_groups_below;
+  
+  bool_t  barrier_sense;
+  lock_t  barrier_lock;
+  thread_count_t   barrier_threads_arrived;
+} thread_comm_t;
+
+void    th_setup_comm( thread_comm_t *comm, 
+				thread_count_t groups_this_level, thread_count_t groups_below );
+void*   th_broadcast_structure( thread_comm_t *comm, void *to_send );
+void    th_barrier( thread_comm_t *comm );
+void    th_set_lock( lock_t *lock );
+void    th_unset_lock( lock_t *lock );
+void    th_init_lock( lock_t *lock );
+void    th_destroy_lock( lock_t *lock );
+
+bool_t  th_am_root( thread_comm_t *comm );
+
+rank_t   th_thread_id( thread_comm_t *comm );
+rank_t   th_group_id( thread_comm_t *comm );
+rank_t   th_global_thread_id();
+
+void th_shift_start_end(dim_t *start, dim_t *end, thread_comm_t *comm);
