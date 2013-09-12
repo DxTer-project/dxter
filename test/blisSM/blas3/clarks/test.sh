c349ff1f6bdfc14829804fd4c340b9a0923885a8
bamarker <bamarker@clarksville.tacc.utexas.edu>
2013-09-12 11:29:22 -0500

    test sh

--- test/blisSM/blas3/clarks/test.sh
+++ test/blisSM/blas3/clarks/test.sh
@@ -2,5 +2,13 @@
 export GOMP_CPU_AFFINITY="0 12 4 16 8 20 1 13 5 17 9 21 2 14 6 18 10 22 3 15 7 19 11 23"
 
 ./test_gemm.x N N | tee results/16/resGemmNN
+
 ./test_trmm.x L L N | tee results/16/resTrmmLLN
-./test_herk.x L N | tee results/16/resHerkLN
+./test_trmm.x R L N | tee results/16/resTrmmRLN
+
+./test_trsm.x L L N | tee results/16/resTrsmLLN
+./test_trsm.x R L N | tee results/16/resTrsmRLN
+
+
+./test_hemm.x L L | tee results/16/resHemmLL
+./test_hemm.x R L | tee results/16/resHemmRL
