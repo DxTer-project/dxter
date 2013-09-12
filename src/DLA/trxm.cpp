c9e82255355e8b05b2082e0fa8edff535943a657
Bryan Marker <bamarker@cs.utexas.edu>
2013-09-12 08:41:02 -0500

    trsm alg fix

--- src/DLA/trxm.cpp
+++ src/DLA/trxm.cpp
@@ -1695,7 +1695,7 @@ Loop* TrsmLoopRightVar1(Node *Ain, unsigned int Anum,
                        NOTUP, FULLUP);
   
   Node *gemm;
-  gemm = new Gemm(layer, NORMAL, trans, coeff, COEFONE, type);
+  gemm = new Gemm(layer, NORMAL, trans, COEF, COEFNEGONE, FONE, type);
   
   if (tri == LOWER) {
     if (trans == NORMAL) {
