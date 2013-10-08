9f75b72d45ce754bc5a57a6a343fadc8c259a439
bamarker <bamarker@clarksville.tacc.utexas.edu>
2013-10-08 17:31:16 -0500

    test.sh

--- test/blisSM/blas3/clarks/test.sh
+++ test/blisSM/blas3/clarks/test.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 export GOMP_CPU_AFFINITY="0 12 4 16 8 20 1 13 5 17 9 21 2 14 6 18 10 22 3 15 7 19 11 23"
 
-./test_gemm.x N N | tee results/16/resGemmNN
-./test_gemm.x T T | tee results/16/resGemmTT
+# ./test_gemm.x N N | tee results/24/resGemmNN
+# ./test_gemm.x T T | tee results/24/resGemmTT
 
-./test_trmm.x L L N | tee results/16/resTrmmLLN
-./test_trmm.x R L N | tee results/16/resTrmmRLN
+# ./test_trmm.x L L N | tee results/24/resTrmmLLN
+# ./test_trmm.x R L N | tee results/24/resTrmmRLN
 
-./test_trsm.x L L N | tee results/16/resTrsmLLN
-./test_trsm.x R L N | tee results/16/resTrsmRLN
+# ./test_trsm.x L L N | tee results/24/resTrsmLLN
+# ./test_trsm.x R L N | tee results/24/resTrsmRLN
 
+# ./test_hemm.x L L | tee results/24/resHemmLL
+# ./test_hemm.x R L | tee results/24/resHemmRL
 
-./test_hemm.x L L | tee results/16/resHemmLL
-./test_hemm.x R L | tee results/16/resHemmRL
+./test_herk.x L N | tee results/24/resHerkLN
+./test_herk.x U N | tee results/24/resHerkUN
+
+./test_her2k.x L N | tee results/24/resHer2kLN
+./test_her2k.x U N | tee results/24/resHer2kUN
