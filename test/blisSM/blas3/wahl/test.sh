79b735897a07ab9093f2d89e93d3ea547410613d
Bryan Marker <bamarker@cs.utexas.edu>
2013-11-24 13:18:19 -0600

    wahlberg BLISSM build

---
+++ test/blisSM/blas3/wahl/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#export GOMP_CPU_AFFINITY="0 12 4 16 8 20 1 13 5 17 9 21 2 14 6 18 10 22 3 15 7 19 11 23"
+export GOMP_CPU_AFFINITY="0 2 1 3"
+
+ ./test_gemm.x N N | tee results/4/resGemmNN
+# ./test_gemm.x T T | tee results/24/resGemmTT
+
+# ./test_trmm.x L L N | tee results/24/resTrmmLLN
+# ./test_trmm.x R L N | tee results/24/resTrmmRLN
+
+# ./test_trsm.x L L N | tee results/24/resTrsmLLN
+# ./test_trsm.x R L N | tee results/24/resTrsmRLN
+
+# ./test_hemm.x L L | tee results/24/resHemmLL
+# ./test_hemm.x R L | tee results/24/resHemmRL
+
+# ./test_herk.x L N | tee results/24/resHerkLN
+# ./test_herk.x U N | tee results/24/resHerkUN
+
+# ./test_her2k.x L N | tee results/24/resHer2kLN
+# ./test_her2k.x U N | tee results/24/resHer2kUN
