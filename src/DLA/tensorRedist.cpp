f2ac82d1ed4ffa29a2e4205903973d4e25b66985
Bryan Marker <bamarker@cs.utexas.edu>
2014-03-10 14:32:59 -0500

    adding tensorRedist

---
+++ src/DLA/tensorRedist.cpp
@@ -0,0 +1,108 @@
+#include "tensorRedist.h"
+
+#if DOTENSORS
+
+RedistNode::RedistNode() 
+{
+  m_destType.SetToDefault(0);
+  m_lsizes = NULL;
+}
+
+RedistNode::RedistNode(const DistType &destType)
+{
+  m_destType = destType;
+  m_lsizes = NULL;
+}
+
+RedistNode::~RedistNode()
+{
+  if (m_lsizes) {
+    delete [] m_lsizes;
+    m_lsizes = NULL;
+  }
+}
+
+void RedistNode::Duplicate(const Node *orig, bool shallow, bool possMerging)
+{
+  DLANode::Duplicate(orig,shallow, possMerging);
+  const RedistNode *origNode = (RedistNode*)orig;
+  m_destType = origNode->m_destType;
+}
+
+NodeType RedistNode::GetType() const
+{
+  if (m_inputs.empty()) {
+    return "RedistNode to " + DistTypeToStr(m_destType) +
+      " without parent";
+  }
+  else {
+    Node *parent = Input(0);
+    DistType type = ((DLANode*)parent)->GetDistType(InputConnNum(0));
+    return  "RedistNode " + DistTypeToStr(type) +
+      " -> " + DistTypeToStr(m_destType);
+  }
+}
+
+void RedistNode::SanityCheck()
+{
+  DLANode::SanityCheck();
+  if (m_inputs.size() != 1) {
+    cout << "m_inputs.size() != 1\n";
+    throw;
+  }
+  
+  if (!m_children.size())
+    throw;
+}
+
+void RedistNode::Prop()
+{
+  if (!IsValidCost(m_cost)) {
+    DLANode *parent = (DLANode*)Input(0);
+    DistType m_srcType = parent->GetDistType(InputConnNum(0));
+    parent->Prop();
+    throw;
+  }
+}
+
+const Dim RedistNode::NumDims(unsigned int num) const
+{
+  if (num > 0)
+    throw;
+  return InputNumDims(0);
+}
+
+const Sizes* RedistNode::Len(unsigned int num, Dim dim) const
+{
+  if (num > 0)
+    throw;
+  return InputLen(0,dim);
+}
+
+const Sizes* RedistNode::LocalLen(unsigned int num, Dim dim) const
+{
+  if (num > 0)
+    throw;
+  return m_lsizes+dim;
+}
+
+void RedistNode::ClearSizeCache()
+{
+  if (m_lsizes) {
+    delete m_lsizes;
+    m_lsizes = NULL;
+  }
+}
+
+void RedistNode::BuildSizeCache()
+{
+  if (m_lsizes)
+    return;
+
+  DLANode *in = (DLANode*)Input(0);
+  unsigned int num = InputConnNum(0);
+  m_lsizes = new Sizes[in->NumDims(num)];
+  GetLocalSizes(m_destType, in->AllLens(num), m_lsizes);
+}
+
+#endif
---
+++ src/DLA/tensorRedist.h
@@ -0,0 +1,65 @@
+/*
+    This file is part of DxTer.
+    DxTer is a prototype using the Design by Transformation (DxT)
+    approach to program generation.
+
+    Copyright (C) 2013, The University of Texas and Bryan Marker
+
+    DxTer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DxTer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.               
+
+    You should have received a copy of the GNU General Public License
+    along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+
+#include "layers.h"
+#include "DLANode.h"
+
+#if DOTENSORS
+
+class RedistNode : public DLANode
+{
+ public:
+  DistType m_destType;
+  SizesArray m_lsizes;
+  RedistNode();
+  RedistNode(const DistType &destType);
+  virtual ~RedistNode();
+  virtual const DistType& GetDistType(unsigned int num) const { return m_destType; }
+  static Node* BlankInst() { return  new RedistNode; }
+  bool KeepsInputVarLive(Node *input, unsigned int numIn, unsigned int &numOut) const {return false;}
+  virtual Node* GetNewInst() { return BlankInst(); }
+  virtual void Duplicate(const Node *orig, bool shallow, bool possMerging);
+  virtual bool IsRedistNode() const {return true;}
+  virtual NodeType GetType() const;
+  virtual void SanityCheck();
+  virtual void Prop();
+  virtual void PrintCode(IndStream &out);
+  virtual bool CanTrans() const;
+  RedistNode* CreateTrans(Trans trans);
+  virtual ClassType GetNodeClass() const {return GetClass();}
+  static ClassType GetClass() {return "redist";}
+  virtual void ClearSizeCache();
+  virtual void BuildSizeCache();
+  virtual const Dim NumDims(unsigned int num) const;
+  virtual const Sizes* Len(unsigned int num, Dim dim) const;
+  virtual const Sizes* LocalLen(unsigned int num, Dim dim) const;
+  virtual const SizesArray AllLens(unsigned int num) const;
+  virtual const SizesArray AllLocalLens(unsigned int num) const;
+  virtual Name GetName(unsigned int num) const;
+  virtual void FlattenCore(ofstream &out) const;
+  virtual void UnflattenCore(ifstream &in, SaveInfo &info);
+  virtual bool Overwrites(const Node *input, unsigned int num) const {return false;}
+};
+
+#endif
