17d9c1b08e4cd3efc32ffc0a8d96b543ae4aa38a
Bryan Marker <bamarker@cs.utexas.edu>
2014-04-09 10:48:40 -0500

    SumScatter in separate file

---
+++ src/DLA/tensorSumScatter.h
@@ -0,0 +1,67 @@
+/*
+    This file is part of DxTer.
+    DxTer is a prototype using the Design by Transformation (DxT)
+    approach to program generation.
+
+    Copyright (C) 2013, The University of Texas and Bryan Marker
+
+    DxTer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DxTer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.               
+
+    You should have received a copy of the GNU General Public License
+    along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+
+#include "layers.h"
+
+#if DOTENSORS
+
+#include "DLANode.h"
+#include "transform.h"
+#include "DLAOp.h"
+
+class SumScatterUpdateNode : public DLAOp<2,1>
+{
+ public:
+  DimSet m_sumDims;
+  Coef m_coef;
+ SumScatterUpdateNode() : DLAOp<2,1>(), m_coef(COEFVALZERO) {}
+  SumScatterUpdateNode(const DimSet &sumDims, Coef coeff);
+  static Node* BlankInst() { return  new SumScatterUpdateNode; }
+  virtual Node* GetNewInst() { return BlankInst(); }
+  virtual void Duplicate(const Node *orig, bool shallow, bool possMerging);
+  virtual bool IsRedistNode() const {return true;}
+  virtual NodeType GetType() const;
+  virtual void SanityCheck();
+  virtual void Prop();
+  virtual void PrintCode(IndStream &out);
+  //  RedistNode* CreateTrans(Trans trans);
+  virtual ClassType GetNodeClass() const {return GetClass();}
+  static ClassType GetClass() {return "redistWithSum";}
+  virtual void FlattenCore(ofstream &out) const;
+  virtual void UnflattenCore(ifstream &in, SaveInfo &info);
+  virtual const DistType& GetDistType(unsigned int num) const { return InputDistType(1); }
+};
+
+/* class SplitSumScatter : public SingleTrans */
+/* { */
+/*  public: */
+/*   Dim m_dim; */
+/*  SplitRedistribs(Dim dim) : m_dim(dim) {} */
+/*   virtual string GetType() const { return (string)"SplitRedist" + (char)(m_dim+48); } */
+/*   virtual bool CanApply(const Poss *poss, const Node *node) const; */
+/*   virtual void Apply(Poss *poss, Node *node) const; */
+/*   virtual bool IsRef() const {return true;} */
+/* }; */
+
+#endif
