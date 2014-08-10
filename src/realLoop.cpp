4f13a837f7996a14ad7b3875e167e72c5894d28f
Bryan Marker <bamarker@cs.utexas.edu>
2014-08-09 22:04:47 -0500

    adding files

---
+++ src/realLoop.cpp
@@ -0,0 +1,495 @@
+/*
+ This file is part of DxTer.
+ DxTer is a prototype using the Design by Transformation (DxT)
+ approach to program generation.
+ 
+ Copyright (C) 2014, The University of Texas and Bryan Marker
+ 
+ DxTer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ DxTer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include "loopSupport.h"
+#include "elemRedist.h"
+#include <cmath>
+#include <climits>
+#include "pack.h"
+#include "critSect.h"
+#include "blis.h"
+
+int RealLoop::M_currLabel = 0;
+
+RealLoop::RealLoop()
+: IntLoop<RealPSet>(), 
+#if TWOD
+ m_dim(BADDIM),
+#endif
+  m_type(UNKNOWNLOOP)
+#if DOBLIS
+, m_comm(CORECOMM)
+#endif
+{
+  AssignNewLabel();
+  m_bsSize = BadBS;
+}
+
+RealLoop::RealLoop(LoopType type)
+: 
+#if TWOD
+  m_dim(BADDIM),
+#endif
+  m_type(type)
+#if DOBLIS
+, m_comm(CORECOMM)
+#endif
+
+{
+#if DOELEM
+  if (m_type == ELEMLOOP)
+    m_bsSize = USEELEMBS;
+  else
+#endif
+    m_bsSize = BadBS;
+  AssignNewLabel();
+}
+
+RealLoop::RealLoop(LoopType type, Poss *poss, BSSize bsSize)
+  :
+  IntLoop<RealPSet>(poss),
+#if TWOD
+ m_dim(BADDIM),
+#endif
+ m_type(type), m_bsSize(bsSize)
+#if DOBLIS
+, m_comm(CORECOMM)
+#endif
+{
+  unsigned int i;
+  for(i = 0; i < poss->m_inTuns.size(); ++i) {
+    LoopTunnel *tun = (LoopTunnel*)(poss->m_inTuns[i]);
+    if (tun->IsSplit()) {
+      SplitBase *split = (SplitBase*)tun;
+      if (split->m_isControlTun)
+        split->m_isControlTun = true;
+    }
+  }
+  AssignNewLabel();
+  
+  bool foundControl = false;
+  NodeVecIter iter = m_inTuns.begin();
+  for(; iter != m_inTuns.end(); ++iter) {
+    Node *in = *iter;
+    if (!in->IsLoopTunnel()) {
+      cout << "non loop tunnel on loop!\n";
+      throw;
+    }
+    if (((LoopTunnel*)in)->IsSplit())
+    {
+      SplitBase *split = (SplitBase*)in;
+      
+      if (split->m_isControlTun) {
+        if (foundControl)
+          throw;
+        else
+          foundControl = true;
+      }
+    }
+  }
+  if (!foundControl)
+    throw;
+}
+
+void RealLoop::Duplicate(const BasePSet *orig, NodeMap &map, bool possMerging, bool useShadows)
+{
+  IntLoop<RealPSet>::Duplicate(orig,map,possMerging,useShadows);
+  const RealLoop *loop = (RealLoop*)orig;
+  m_label = loop->m_label;
+  m_bsSize = loop->m_bsSize;
+  m_type = loop->m_type;
+#if DOBLIS
+  m_comm = loop->m_comm;
+#endif
+#if TWOD
+  m_dim = loop->m_dim;
+#endif
+  if (GetBS() == 0) {
+    cout << "duplicating a loop with zero blocksize\n";
+    throw;
+  }
+}
+
+void RealLoop::AssignNewLabel()
+{
+  m_label.insert(M_currLabel++);
+}
+
+void RealLoop::SetBS(BSSize size)
+{
+  m_bsSize = size;
+}
+
+int RealLoop::GetBS() const
+{
+  return (int)(m_bsSize.GetSize());
+}
+
+void RealLoop::FlattenCore(ofstream &out) const
+{
+  WRITE(m_type);
+  WRITE(m_bsSize);
+#if DOBLIS
+  WRITE(m_comm);
+#endif
+#if TWOD
+  WRITE(m_dim);
+#endif
+  unsigned int size = m_label.size();
+  WRITE(size);
+  IntSetConstIter iter = m_label.begin();
+  for(; iter != m_label.end(); ++iter)
+    WRITE(*iter);
+}
+
+
+void RealLoop::UnflattenCore(ifstream &in, SaveInfo &info)
+{
+  READ(m_type);
+  READ(m_bsSize);
+#if DOBLIS
+  READ(m_comm);
+#endif
+#if TWOD
+  READ(m_dim);
+#endif
+  unsigned int size;
+  READ(size);
+  for(unsigned int i = 0; i < size; ++i) {
+    int tmp;
+    READ(tmp);
+    m_label.insert(tmp);
+  }
+}
+
+void RealLoop::FlattenStatic(ofstream &out)
+{
+  WRITE(M_currLabel);
+}
+
+void RealLoop::UnflattenStatic(ifstream &in)
+{
+  READ(M_currLabel);
+}
+
+void RealLoop::FillTunnelSizes()
+{
+  SplitBase *control = GetControl();
+  if (!control)
+    throw;
+  bool upToDate = true;
+  NodeVecIter iter = m_inTuns.begin();
+  for(; iter != m_inTuns.end(); ++iter) {
+    LoopTunnel *tun = (LoopTunnel*)(*iter);
+#if TWOD
+    if (!tun->m_msizes) {
+#else
+    if (!tun->m_sizes) {
+#endif
+      upToDate = false;
+      break;
+    }
+  }
+  if (upToDate)
+    return;
+  iter = m_inTuns.begin();
+  for(; iter != m_inTuns.end(); ++iter) {
+    LoopTunnel *tun = (LoopTunnel*)(*iter);
+    tun->ClearDataTypeCache();
+  }
+  iter = m_inTuns.begin();
+  for(; iter != m_inTuns.end(); ++iter) {
+    Node *in = *iter;
+    if (!in->IsLoopTunnel())
+      throw;
+    ((LoopTunnel*)in)->StartFillingSizes();
+  }
+  unsigned int numExecs = control->NumberOfLoopExecs();
+  for(unsigned int i = 0; i < numExecs; ++i) {
+    unsigned int numIters = control->NumIters(i);
+    if (numIters) {
+      iter = m_inTuns.begin();
+      for (; iter != m_inTuns.end(); ++iter) {
+        LoopTunnel *in = (LoopTunnel*)(*iter);
+#if DOBLIS
+        in->AppendSizes(i, numIters, NumGroupsInComm(m_comm));
+#else
+        in->AppendSizes(i, numIters, 1);
+#endif
+      }
+    }
+  }
+#if DODM
+  iter = m_inTuns.begin();
+  for (; iter != m_inTuns.end(); ++iter) {
+    LoopTunnel *in = (LoopTunnel*)(*iter);
+    in->UpdateLocalSizes();
+  }
+#endif
+}
+
+void RealLoop::BuildDataTypeCache()
+{
+  FillTunnelSizes();
+  IntLoop<RealPSet>::BuildDataTypeCache();
+}
+
+
+void HardDeleteNode(Node *node)
+{
+  node->m_poss->RemoveFromGraphNodes(node);
+  NodeConnVecIter iter = node->m_children.begin();
+  for(; iter != node->m_children.end(); ++iter)
+    delete *iter;
+  node->m_children.clear();
+  iter = node->m_inputs.begin();
+  for(; iter != node->m_inputs.end(); ++iter)
+    delete *iter;
+  node->m_inputs.clear();
+}
+
+void RealLoop::TryToDeleteLoopTunnelSetAndCleanUp(LoopTunnel *tun)
+{
+  if (!m_shadows.empty())
+    throw;
+  if (tun->GetNodeClass() != LoopTunnel::GetClass()) {
+    cout << "only handling LoopTunnels specifically, not splits\n";
+    throw;
+  }
+  if (tun->m_tunType != POSSTUNIN)
+    throw;
+  
+  LoopTunnel *setTunIn = (LoopTunnel*)(tun->Input(0));
+  
+  //If there is an poss that uses the LoopTunnel's input,
+  // then we shouldn't remove it from all
+  //Check all children of the set input (poss inputs)
+  // by checking all of their children - if all children's
+  // children are TunOuts, then we're good.
+  NodeConnVecIter childIter = setTunIn->m_children.begin();
+  for(; childIter != setTunIn->m_children.end(); ++childIter) {
+    Node *child = (*childIter)->m_n;
+    NodeConnVecIter childIter2 = child->m_children.begin();
+    for(; childIter2 != child->m_children.end(); ++childIter2) {
+      Node *childChild = (*childIter2)->m_n;
+      if (!childChild->IsTunnel(POSSTUNOUT))
+        return;
+    }
+  }
+  
+  //At this point, we've checked, and there is no reason
+  // to keep this set of loop tunnels around
+  unsigned int tunNum = UINT_MAX;
+  for(unsigned int i = 0; i < m_inTuns.size(); ++i) {
+    if (m_inTuns[i] == setTunIn) {
+      tunNum = i;
+      break;
+    }
+  }
+  if (tunNum == UINT_MAX)
+    throw;
+  
+  Node *setTunOut = m_outTuns[tunNum];
+  
+  if (setTunOut->m_children.size() > 0)
+    throw;
+  
+  NodeConnVecIter outIter = setTunOut->m_inputs.begin();
+  for(; outIter != setTunOut->m_inputs.end(); ++outIter) {
+    HardDeleteNode((*outIter)->m_n);
+  }
+  
+  m_outTuns.erase(m_outTuns.begin()+tunNum);
+  HardDeleteNode(setTunOut);
+  
+  childIter = setTunIn->m_children.begin();
+  for(; childIter != setTunIn->m_children.end(); ++childIter) {
+    Node *child = (*childIter)->m_n;
+    HardDeleteNode(child);
+    delete *childIter;
+  }
+  setTunIn->m_children.clear();
+  
+  m_inTuns.erase(m_inTuns.begin()+tunNum);
+  
+  setTunIn->m_poss->DeleteChildAndCleanUp(setTunIn, true);
+}
+
+#if DOBLIS
+void RealLoop::Parallelize(Comm comm)
+{
+  if (NumGroupsInComm(comm) <= 1)
+    throw;
+  if (RemoveParallelization(comm))
+    throw;
+  m_comm = comm;
+  m_hasProped = false;
+  if (!HasIndepIters()) {
+    bool found = false;
+    //If this code changes, reflace in OnlyParallelizedOnNonIndependentData
+    NodeVecIter iter = m_inTuns.begin();
+    for(; iter != m_inTuns.end(); ++iter) {
+      LoopTunnel *tun = (LoopTunnel*)(*iter);
+      if (!tun->IndepIters() && !tun->InputIsTemp()) {
+        if (found)
+          throw;
+        found = true;
+        unsigned int numOut = tun->NumOutputs();
+        if (tun->IsSplit()) {
+	  if (tun->GetNodeClass() != SplitSingleIter::GetClass())
+	    throw;
+	  --numOut;
+	}
+        int i;
+        for(i = 0; i < (int)(tun->m_children.size()); ++i) {
+          Node *possTun = (tun->m_children[i])->m_n;
+          Poss *poss = possTun->m_poss;
+          NodeSet nodeSet;
+          for (unsigned int j = 0; j < numOut; ++j) {
+            AddUsersOfLiveOutput(possTun, j, nodeSet);
+          }
+          if (!nodeSet.size())
+            throw;
+          poss->FillClique(nodeSet);
+	  throw;
+#if 0
+          CritSect *crit = (CritSect*)(poss->FormSetForClique(nodeSet, true));
+          if (crit->RemoveParallelization(CORECOMM)) {
+            //This critical section is around some hierarchy of PSets
+            // from which parallel code cannot be removed without getting
+            // rid of all code
+            RemoveAndDeletePoss(poss, true);
+            --i;
+          }
+          if (HasParallelCode(crit->m_posses[0])) {
+            throw;
+          }
+#endif
+        }
+      }
+    }
+  }
+  
+  ClearDataTypeCache();
+  
+  //If we're parallelizing a loop that is on a poss
+  // that just got duplicated as part of a transformation,
+  // then that duplicated poss doesn't have its size cache.
+  //We need to form it and this loop's size cache (which will be
+  // different thanks to paralellization.
+  if (m_ownerPoss)
+    m_ownerPoss->BuildDataTypeCache();
+}
+
+bool ContainsOnlyParallelization(PSet *set)
+{
+  if (!set->IsReal())
+    return ContainsOnlyParallelization(set->GetReal());
+  if (set->IsLoop()) {
+    Loop *loop = (Loop*)set;
+    if (loop->IsParallel()) {
+      return true;
+    }
+  }
+  
+  PossMMapIter iter = set->m_posses.begin();
+  for(; iter != set->m_posses.end(); ++iter) {
+    Poss *poss = (*iter).second;
+    bool foundParSet = false;
+    PSetVecIter setIter = poss->m_sets.begin();
+    for(; !foundParSet && setIter != poss->m_sets.end(); ++setIter) {
+      PSet *set = *setIter;
+      if (ContainsOnlyParallelization(set))
+        foundParSet = true;
+    }
+    if (!foundParSet) {
+      bool foundParNode = false;
+      NodeVecIter nodeIter = poss->m_possNodes.begin();
+      for(; !foundParNode && nodeIter != poss->m_possNodes.end(); ++nodeIter) {
+        if ((*nodeIter)->IsParallel()) {
+          foundParNode = true;
+        }
+      }
+      if (!foundParNode)
+        return false;
+    }
+  }
+  return true;
+}
+
+bool ContainsOnlyParallelization(const NodeSet &set)
+{
+  PSetSet setSet;
+  NodeSetIter iter = set.begin();
+  for(; iter != set.end(); ++iter) {
+    Node *node = *iter;
+    if (node->IsParallel())
+      return true;
+    if (node->IsTunnel(SETTUNIN)) {
+      Tunnel *tun = (Tunnel*)node;
+      if (setSet.find(tun->m_pset) == setSet.end()) {
+        setSet.insert(tun->m_pset);
+        if (ContainsOnlyParallelization(tun->m_pset))
+          return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool RealLoop::HasIndepIters() const
+{
+  NodeVecConstIter iter = m_inTuns.begin();
+  for (; iter != m_inTuns.end(); ++iter) {
+    const LoopTunnel *in = (LoopTunnel*)(*iter);
+    if (!in->IndepIters()) {
+      return false;
+    }
+  }
+  return true;
+}
+#endif
+
+#if TWOD
+ void RealLoop::SetDimName(DimName dim)
+ {
+   m_functionality += (char)(48+dim);
+   m_dim = dim;
+ }
+#endif
+
+
+
+ShadowPSet* RealLoop::GetNewShadow()
+{
+  ShadowLoop *shadow = new ShadowLoop;
+  shadow->m_realPSet = this;
+  m_shadows.push_back(shadow);
+  return shadow;
+}
+
+
+ RealLoop::~RealLoop()
+ {
+   Migrate();
+ }
---
+++ src/realLoop.h
@@ -0,0 +1,86 @@
+
+/*
+    This file is part of DxTer.
+    DxTer is a prototype using the Design by Transformation (DxT)
+    approach to program generation.
+
+    Copyright (C) 2014, The University of Texas and Bryan Marker
+
+    DxTer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DxTer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.               
+
+    You should have received a copy of the GNU General Public License
+    along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#pragma once
+
+#include "base.h"
+#include "transform.h"
+#include "basePSet.h"
+#include "comm.h"
+#include "intLoop.h"
+#include "realPSet.h"
+
+class RealLoop : public IntLoop<RealPSet>
+{
+ private:
+#if TWOD
+  DimName m_dim;
+#endif
+
+ public:
+  //  typedef int Size;
+  LoopType m_type;
+  static int M_currLabel;
+ public:
+  IntSet m_label;
+  BSSize m_bsSize;
+#if DOBLIS
+  Comm m_comm;
+#endif
+  
+  RealLoop();
+  RealLoop(LoopType type);
+  RealLoop(LoopType type, Poss *poss, BSSize bsSize);
+  virtual ~RealLoop();
+  virtual BasePSet* GetNewInst() {return (BasePSet*)(new RealLoop(m_type));}
+  virtual ShadowPSet* GetNewShadow();
+
+  virtual const IntSet& GetLabel() const {return m_label;}
+  virtual LoopType GetType() const {return m_type;}
+  virtual void Duplicate(const BasePSet *orig, NodeMap &map, bool possMerging, bool useShadows);
+  void AssignNewLabel();
+  void SetBS(BSSize size);
+  int GetBS() const;
+  virtual BSSize GetBSSize() const {return m_bsSize;}
+  void TryToDeleteLoopTunnelSetAndCleanUp(LoopTunnel *tun);
+#if TWOD
+  void SetDimName(DimName dim);
+  DimName GetDimName() const {return m_dim;}
+#endif
+
+  void FillTunnelSizes();
+  virtual void BuildDataTypeCache();
+
+  virtual void FlattenCore(ofstream &out) const;
+  virtual void UnflattenCore(ifstream &in, SaveInfo &info);
+  static void FlattenStatic(ofstream &out);
+  static void UnflattenStatic(ifstream &in);
+
+#if DOBLIS
+  void Parallelize(Comm comm);
+  virtual bool HasIndepIters() const;
+  bool IsParallel() const {return m_comm!=CORECOMM;}
+#endif
+
+};
