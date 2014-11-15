d0324b37fc5ab002a13bf62e181fb126d51d2a1f
Dillon Huff <devnull@localhost>
2014-11-15 16:06:30 -0600

    Added code for basic scalar arithmetic

---
+++ src/LLDLA/scalarArith.cpp
@@ -0,0 +1,77 @@
+/*
+    This file is part of DxTer.
+    DxTer is a prototype using the Design by Transformation (DxT)
+    approach to program generation.
+
+    Copyright (C) 2014, The University of Texas and Bryan Marker
+
+    DxTer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DxTer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.               
+
+    You should have received a copy of the GNU General Public License
+    along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "scalarArith.h"
+
+#if DOLLDLA
+
+void AddScalars::Prop()
+{
+  if (!IsValidCost(m_cost)) {
+    DLAOp<2, 1>::Prop();
+     m_cost = 1;
+  }
+}
+
+void AddScalars::PrintCode(IndStream &out)
+{
+  out.Indent();
+  string aStr = GetInputNameStr(0);
+  string bStr = GetInputNameStr(1);
+  *out << bStr << " = " << bStr << " + " << aStr << ";\n";
+  return;
+}
+
+void MulScalars::Prop()
+{
+  if (!IsValidCost(m_cost)) {
+    DLAOp<2, 1>::Prop();
+    m_cost = 1;
+  }
+}
+
+void MulScalars::PrintCode(IndStream &out)
+{
+  out.Indent();
+  string aStr = GetInputNameStr(0);
+  string bStr = GetInputNameStr(1);
+  *out << bStr << " = " << bStr << " * " << aStr << ";\n"; 
+  return;
+}
+
+void SetScalarToZero::Prop()
+{
+  if (!IsValidCost(m_cost)) {
+    DLAOp<2, 1>::Prop();
+    m_cost = 1;
+  }
+  return;
+}
+
+void SetScalarToZero::PrintCode(IndStream &out)
+{
+  out.Indent();
+  string aStr = GetInputNameStr(0);
+  *out << aStr << " = 0.0;\n";
+  return;
+}
+
+#endif // DOLLDLA
---
+++ src/LLDLA/scalarArith.h
@@ -0,0 +1,78 @@
+/*
+    This file is part of DxTer.
+    DxTer is a prototype using the Design by Transformation (DxT)
+    approach to program generation.
+
+    Copyright (C) 2014, The University of Texas and Bryan Marker
+
+    DxTer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DxTer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.               
+
+    You should have received a copy of the GNU General Public License
+    along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "DLANode.h"
+#include "DLAOp.h"
+#include "LLDLA.h"
+
+#if DOLLDLA
+
+class AddScalars : public DLAOp<2, 1>
+{
+ public:
+  virtual NodeType GetType() const { return "AddScalars"; }
+  static Node* BlankInst() { return new AddScalars(); }
+  virtual Node* GetNewInst() { return BlankInst(); }
+
+  virtual void Prop();
+  virtual void PrintCode(IndStream &out);
+  virtual ClassType GetNodeClass() const { return GetClass(); }
+  static ClassType GetClass() { return "AddScalars"; }
+
+  virtual bool IsReadOnly() const { return false; }
+  virtual bool IsDataDependencyOfInput() const { return true; }
+};
+
+class MulScalars : public DLAOp<2, 1>
+{
+ public:
+  virtual NodeType GetType() const { return "MulScalars"; }
+  static Node* BlankInst() { return new MulScalars(); }
+  virtual Node* GetNewInst() { return BlankInst(); }
+
+  virtual void Prop();
+  virtual void PrintCode(IndStream &out);
+  virtual ClassType GetNodeClass() const { return GetClass(); }
+  static ClassType GetClass() { return "MulScalars"; }
+
+  virtual bool IsReadOnly() const { return false; }
+  virtual bool IsDataDependencyOfInput() const { return true; }
+};
+
+class SetScalarToZero : public DLAOp<1, 1>
+{
+ public:
+  virtual NodeType GetType() const { return "SetScalarToZero"; }
+  static Node* BlankInst() { return new SetScalarToZero(); }
+  virtual Node* GetNewInst() { return BlankInst(); }
+
+  virtual void Prop();
+  virtual void PrintCode(IndStream &out);
+  virtual ClassType GetNodeClass() const { return GetClass(); }
+  static ClassType GetClass() { return "SetScalarToZero"; }
+
+  virtual bool IsReadOnly() const { return false; }
+  virtual bool IsDataDependencyOfInput() const { return true; } 
+};
+
+#endif // DOLLDLA
