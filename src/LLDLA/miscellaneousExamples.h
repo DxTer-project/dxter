22ce407a0c4531a22d8708f99cb992ec607ddbfe
Dillon Huff <devnull@localhost>
2014-12-30 20:58:57 -0600

    Moved miscellaneous (not single operation or BLAS) examples to their own file

---
+++ src/LLDLA/miscellaneousExamples.h
@@ -0,0 +1,21 @@
+#include "DLAReg.h"
+#include "LLDLA.h"
+#include "madd.h"
+#include "mvmul.h"
+#include "partition.h"
+#include "recombine.h"
+#include "svmul.h"
+#include "vadd.h"
+#include "vvdot.h"
+
+#if DOLLDLA
+
+RealPSet* GenSizeColSVMul(Type dataType, int m);
+RealPSet* TransMVMulExample(Type dataType, int m, int n);
+RealPSet* MVMul2Example(Type dataType, int m, int n, int p);
+RealPSet* MAdd2Example(Type dataType, int m, int n);
+RealPSet* VAdd2Example(Type dataType, int m);
+RealPSet* VMVMulExample(Type dataType, int m, int n);
+RealPSet* DoubleGemmExample(Type dataType, Trans transA, Trans transB, int m, int n, int p, int k);
+
+#endif // DOLLDLA
