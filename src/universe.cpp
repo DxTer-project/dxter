17921ba2209c46cf3f8ee7354728d7066d5dde44
Bryan Marker <bamarker@cs.utexas.edu>
2015-02-09 10:59:38 -0600

    minor

--- src/universe.cpp
+++ src/universe.cpp
@@ -813,6 +813,7 @@ void Universe::EnforceMemConstraint(Cost maxMem)
   StrSet blank;
   if (m_pset->EnforceMemConstraint(maxMem, blank)) {
     cout << "Base PSet is over memory\n";
+    cout.flush();
     throw;
   }
 }
