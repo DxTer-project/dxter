1b08b88e6e45a2cbae8438a17b32df2674661a6b
Dillon Huff <dillonhuff@gmail.com>
2015-03-11 08:13:29 -0500

    vaddSplitToMainAndResidual runs, but causes an error in LOOPPHASE

--- src/LLDLA/vaddSplitToMainAndResidual.cpp
+++ src/LLDLA/vaddSplitToMainAndResidual.cpp
@@ -19,6 +19,47 @@
     along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "vaddSplitToMainAndResidual.h"
+
 #if DOLLDLA
+
+#include "base.h"
+#include "packingUtils.h"
+#include "VAdd.h"
+
+VAddSplitToMainAndResidual::VAddSplitToMainAndResidual(Layer fromLayer, Layer toLayer, VecType vecType) {
+  m_fromLayer = fromLayer;
+  m_toLayer = toLayer;
+  m_vecType = vecType;
+}
+
+bool VAddSplitToMainAndResidual::CanApply(const Node* node) const {
+  if (node->GetNodeClass() == VAdd::GetClass()) {
+    const VAdd* vadd = static_cast<const VAdd*>(node);
+    if (vadd->GetLayer() != m_fromLayer) {
+      return false;
+    }
+    if (vadd->GetVecType() != m_vecType) {
+      return false;
+    }
+    if (m_vecType == ROWVECTOR) {
+      return !(vadd->InputNIsMultipleOfVecRegWidth(0))
+	&& (vadd->GetInputNumCols(0) > vadd->GetVecRegWidth());
+    } else {
+      return !(vadd->InputMIsMultipleOfVecRegWidth(0))
+	&& (vadd->GetInputNumRows(0) > vadd->GetVecRegWidth());
+    }
+  }
+  throw;
+}
+
+void VAddSplitToMainAndResidual::Apply(Node* node) const {
+  DimName splitDim = m_vecType == ROWVECTOR ? DIMN : DIMM;
+  auto unpack = SplitBinarySymmetricOperationIntoMainAndResidual(m_toLayer, node, splitDim, node->GetVecRegWidth());
+  node->m_poss->AddUp(node->m_poss->m_possNodes, unpack, false, true);
+  node->RedirectChildren(unpack, 0);
+  node->m_poss->DeleteChildAndCleanUp(node);
+  return;
+}
 
 #endif // DOLLDLA
