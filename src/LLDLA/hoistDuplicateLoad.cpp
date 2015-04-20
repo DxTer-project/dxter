23a6e9651764b5ecebdda8d5db774ae8094d35ad
Bryan Marker <bamarker@cs.utexas.edu>
2015-04-20 12:34:54 -0500

    fixing bug with multiple loads being hoisted

--- src/LLDLA/hoistDuplicateLoad.cpp
+++ src/LLDLA/hoistDuplicateLoad.cpp
@@ -1,23 +1,23 @@
 /*
-    This file is part of DxTer.
-    DxTer is a prototype using the Design by Transformation (DxT)
-    approach to program generation.
-
-    Copyright (C) 2015, The University of Texas and Bryan Marker
-
-    DxTer is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    DxTer is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ This file is part of DxTer.
+ DxTer is a prototype using the Design by Transformation (DxT)
+ approach to program generation.
+ 
+ Copyright (C) 2015, The University of Texas and Bryan Marker
+ 
+ DxTer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ DxTer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with DxTer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "hoistDuplicateLoad.h"
 
@@ -41,10 +41,10 @@ bool HoistDuplicateLoad::PossChildrenAreOnlyLoads(const LoopTunnel* possTunIn) c
   for(auto tunChildConn : possTunIn->m_children) {
     if (!tunChildConn->m_n->IsTunnel(POSSTUNOUT)) {
       if (tunChildConn->m_n->GetNodeClass() == DuplicateRegLoad::GetClass()) {
-	foundLoad = true;
+        foundLoad = true;
       }
       else {
-	foundSomethingElse = true;
+        foundSomethingElse = true;
       }
     }
   }
@@ -56,17 +56,17 @@ bool HoistDuplicateLoad::CanApply(const Node *node) const
   if (node->GetNodeClass() != LoopTunnel::GetClass()) {
     throw;
   }
-
+  
   auto setTunIn = static_cast<const LoopTunnel*>(node);
-
+  
   if (setTunIn->m_tunType != SETTUNIN) {
     return false;
   }
-
+  
   if (!setTunIn->m_pset->IsReal()) {
     throw;
   }
-
+  
   return OnlyFoundLoad(setTunIn);
 }
 
@@ -76,29 +76,30 @@ void HoistDuplicateLoad::RemoveLoadsFromPosses(LoopTunnel* setTunIn) const {
     auto possTunIn = static_cast<LoopTunnel*>(setTunIn->Child(possNum));
     auto poss = possTunIn->m_poss;
     if (PossChildrenAreOnlyLoads(possTunIn)) {
-      for(auto tunChildConn : possTunIn->m_children) {
-	if (tunChildConn == NULL) {
-	  cout << "tunChildConn is null" << endl;
-	  throw;
-	}
-	if (tunChildConn->m_n == NULL) {
-	  cout << "tunChildConn->m_n is null" << endl;
-	  throw;
-	}
-	auto tunChildConnNode = tunChildConn->m_n;
-	if (!tunChildConnNode->IsTunnel(POSSTUNOUT)) {
-	  auto oldLoad = static_cast<DuplicateRegLoad*>(tunChildConn->m_n);
-	  oldLoad->RedirectChildren(possTunIn, 0);
-	  poss->DeleteChildAndCleanUp(oldLoad);
-	} else {
-	  LoopTunnel *possTunOut = static_cast<LoopTunnel*>(tunChildConn->m_n);
-	  for(auto inputConn : possTunOut->m_inputs) {
-	    if (!inputConn->m_n->IsTunnel(POSSTUNIN)) {
-	      throw;
-	      //handle this case by removing any stores
-	    }
-	  }
-	}
+      NodeConnVec tmp = possTunIn->m_children;
+      for(auto tunChildConn : tmp) {
+        if (tunChildConn == NULL) {
+          cout << "tunChildConn is null" << endl;
+          throw;
+        }
+        if (tunChildConn->m_n == NULL) {
+          cout << "tunChildConn->m_n is null" << endl;
+          throw;
+        }
+        auto tunChildConnNode = tunChildConn->m_n;
+        if (!tunChildConnNode->IsTunnel(POSSTUNOUT)) {
+          auto oldLoad = static_cast<DuplicateRegLoad*>(tunChildConn->m_n);
+          oldLoad->RedirectChildren(possTunIn, 0);
+          poss->DeleteChildAndCleanUp(oldLoad);
+        } else {
+          LoopTunnel *possTunOut = static_cast<LoopTunnel*>(tunChildConn->m_n);
+          for(auto inputConn : possTunOut->m_inputs) {
+            if (!inputConn->m_n->IsTunnel(POSSTUNIN)) {
+              throw;
+              //handle this case by removing any stores
+            }
+          }
+        }
       }
     } else {
       set->RemoveAndDeletePoss(poss, true);
@@ -119,10 +120,10 @@ void HoistDuplicateLoad::Apply(Node *node) const {
   if (!setTunIn->m_pset->IsReal()) {
     throw;
   }
-
+  
   RemoveLoadsFromPosses(setTunIn);
   AddOneOuterLoad(setTunIn);
-
+  
   if (setTunIn->GetMatchingOutTun()->m_children.size()) {
     //handle this case by adding a store between the set tun out and its children
     throw;
